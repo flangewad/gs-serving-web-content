4fbbf9ef9f2f07758db71504d838785b96f714ca
Katie Moulang <kmoulang@pivotal.io>
2016-04-25 14:06:35 +0100

    Hardcode jar move to put folder

--- complete/build.sh
+++ complete/build.sh
@@ -6,7 +6,7 @@ export TERM=${TERM:-dumb} # needed for Gradle: https://issues.gradle.org/browse/
 pushd source-code/complete
 ./gradlew build
 mkdir package
-cp build/libs/gs-serving-web-content-0.1.0.jar package
+cp build/libs/gs-serving-web-content-0.1.0.jar /tmp/build/put/source-code/complete/build/libs/
 popd
 
 
