e0f11e61181ac34632cf640b6d4f851a84ac7066
Katie Moulang <kmoulang@pivotal.io>
2016-04-25 14:21:17 +0100

    Try moving jar to higher up package directory
    Signed-off-by: Rosie Bloxsom <rbloxsom@pivotal.io>

--- complete/build.sh
+++ complete/build.sh
@@ -5,11 +5,7 @@ export TERM=${TERM:-dumb} # needed for Gradle: https://issues.gradle.org/browse/
 
 pushd source-code/complete
 ./gradlew build
-mkdir package
-cp build/libs/gs-serving-web-content-0.1.0.jar /tmp/build/put/source-code/complete/build/libs/
 popd
 
-
-
-
-
+cp source-code/complete/build/libs/gs-serving-web-content-0.1.0.jar package
+cp source-code/complete/manifest.yml package
