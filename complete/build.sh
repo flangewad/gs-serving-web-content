18c67d5e394c3305b51d6fead7ac3b725e18d651
Katie Moulang <kmoulang@pivotal.io>
2016-04-25 12:02:08 +0100

    Add cf deploy to pipeline

--- complete/build.sh
+++ complete/build.sh
@@ -5,4 +5,10 @@ export TERM=${TERM:-dumb} # needed for Gradle: https://issues.gradle.org/browse/
 
 pushd source-code/complete
 ./gradlew build
+cf login -a api.run.pivotal.io -u kmoulang@pivotal.io -o pivotallabs -s Beach-London
 popd
+
+
+
+
+
