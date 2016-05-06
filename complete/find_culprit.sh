94634a5e0afbaf86d7c8315d7f8f429593b012d6
Rosie Bloxsom <rbloxsom@pivotal.io>
2016-05-06 16:43:13 +0100

    Add culprit-finding script, and add ifttt to pipeline
    Signed-off-by: Katie Moulang <kmoulang@pivotal.io>

---
+++ complete/find_culprit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e -x
+export TERM=${TERM:-dumb} # needed for Gradle: https://issues.gradle.org/browse/GRADLE-2634
+
+pushd source-code/
+git --no-pager show -s --format='%an <%ae>'
+popd
+
+cp source-code/complete/build/libs/gs-serving-web-content-0.1.0.jar package
+cp source-code/complete/manifest.yml package
